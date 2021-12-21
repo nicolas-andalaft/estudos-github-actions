77b83024e9cc1edb33213550c8b5a62debe1edaf
Nicolas Andalaft <nicolas.andalaft@hotmail.com>
2021-12-21 16:36:31 -0300

    action editada

--- src/action.js
+++ src/action.js
@@ -2,21 +2,17 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run() {
-    const github_token = core.getInput('GITHUB_TOKEN');
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const octokit = github.getOctokit(GITHUB_TOKEN);
 
-    const context = github.context;
-    if (context.payload.pull_request == null) {
-        core.setFailed('No pull request found.');
-        return;
-    }
-    const pull_request_number = context.payload.pull_request.number;
+    const { context = {} } = github;
+    const { pull_request } = context.payload;
 
-    const octokit = new github.getOctokit(github_token);
     await octokit.issues.createComment({
-        ...context.repo,
-        issue_number: pull_request_number,
+        ...github.context.repo,
+        issue_number: github.context.payload.pull_request.number,
         body: 'Thank you for submitting a pull request! We will try to review this as soon as we can'
-      }); 
+    })
 }
 
 run();
