5e2a608784833686cbc9421c848c081e37349455
Nicolas Andalaft <nicolas.andalaft@hotmail.com>
2021-12-21 15:27:16 -0300

    Correçao na chamada errada de funçao

--- src/action.js
+++ src/action.js
@@ -8,7 +8,7 @@ async function run() {
     const { context = {} } = github;
     const { pull_request } = context.payload;
 
-    await octokit.createComment({
+    await octokit.issues.createComment({
         ...context.repo,
         issue_number: pull_request.number,
         body: 'Thank you for submitting a pull request! We will try to review this as soon as we can'
